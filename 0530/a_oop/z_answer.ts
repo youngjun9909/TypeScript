9d80a6c9108521f4ba00e5e7a7c896d4d7196ea5
youngjun9909 <165034190+youngjun9909@users.noreply.github.com>
2024-07-15 18:51:54 +0900

    Add files via upload

---
+++ 0530/a_oop/z_answer.ts
@@ -0,0 +1,17 @@
+//! Library 클래스 정의
+
+//! 도서관 관리 시스템 구현
+// Library 클래스 정의
+// 1. 속성
+//  books: 도서들의 정보가 저장된 배열
+// 2. 생성자
+//  : 초기 도서 배열을 받아 books 배열을 초기화
+//  : 전달 받는 인자가 없을 경우 [] 빈 배열로 초기화
+// 3. 메서드
+// addBook(book: BookType): 책 추가
+// removeBook(isbn: string): isbn으로 책을 찾아 제거, 해당 책이 없는 경우 메시지 출력
+// findBooksByAuthor(author: string): 저자명으로 책을 찾아 배열로 반환
+// listBooks(): 도서 목록 정보 출력(전체 리스트 출력)
+class Library {
+  
+}
